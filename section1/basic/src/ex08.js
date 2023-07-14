e72b015cb558ed86e67fa74327e427dbff4e780d
jjjjjeongwon <jjjjjwon21@gmail.com>
2023-07-15 04:10:23 +0900

    feat: 라이브러리를 이용한 애니메이션 구현(gsap)

---
+++ section1/basic/src/ex08.js
@@ -0,0 +1,90 @@
+// ex07 - 라이브러리를 이용한 애니메이션
+import * as THREE from 'three';
+import gsap from 'gsap';
+
+export default function example() {
+  // 동적으로 캔버스 조립하기
+  // const renderer = new THREE.WebGL1Renderer();
+  // renderer.setSize(window.innerWidth, window.innerHeight);
+  // document.body.appendChild(renderer.domElement);
+
+  const canvas = document.querySelector('#three-canvas');
+  const renderer = new THREE.WebGLRenderer({
+    canvas,
+    antialias: true,
+    // alpha: true,
+  });
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  // console.log(window.devicePixelRatio);
+  renderer.setPixelRatio(window.devicePixelRatio > 1 ? 2 : 1);
+
+  // renderer.setClearColor('#00ff00');
+  // renderer.setClearAlpha(0.5);
+
+  //Scene
+  const scene = new THREE.Scene();
+  scene.fog = new THREE.Fog('black', 3, 7); // Fog(color, near, far)
+
+  // Camera
+  const camera = new THREE.PerspectiveCamera(
+    75, //시야각
+    window.innerWidth / window.innerHeight, //종횡비
+    0.1, //near
+    1000 //far
+  );
+
+  camera.position.y = 1;
+  camera.position.z = 5;
+  scene.add(camera);
+
+  const light = new THREE.DirectionalLight(0xffffff, 1);
+  light.position.x = 1;
+  light.position.y = 3;
+  light.position.z = 5;
+
+  scene.add(light);
+
+  //Mesh
+  const geometry = new THREE.BoxGeometry(1, 1, 1);
+  const material = new THREE.MeshStandardMaterial({
+    color: 'red',
+  });
+
+  const mesh = new THREE.Mesh(geometry, material);
+  scene.add(mesh);
+
+  //그리기
+  let oldTime = Date.now();
+
+  function draw() {
+    const newTime = Date.now();
+    const deltaTime = newTime - oldTime;
+    oldTime = newTime;
+
+    renderer.render(scene, camera);
+
+    // 같은 기능
+    // window.requestAnimationFrame(draw);
+    renderer.setAnimationLoop(draw);
+  }
+
+  function setSize() {
+    //카메라
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.render(scene, camera);
+  }
+
+  //gsap
+  gsap.to(mesh.position, {
+    duration: 0.5,
+    y: 1,
+    z: 3,
+  });
+
+  //이벤트
+  window.addEventListener('resize', setSize);
+
+  draw();
+}
